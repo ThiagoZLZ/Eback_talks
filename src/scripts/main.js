e434ec1e6b8f3140fb2ace1fb5cb38b1eb9f64ea
Thiago Carvalho <thiago.carvalho1991@hotmail.com>
2024-03-22 22:50:33 -0300

    Criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-console.log("Olá mundo");
+AOS.init();
+
+const dataDoEvento = new Date("Mar 22, 2024 22:42:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contandoHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutosEmMS = 1000 * 60;
+
+   const diasAteEvento = Math.floor(distanciaAteEvento / diaEmMs);
+   const horasAteEvento = Math.floor((distanciaAteEvento % diaEmMs) / horaEmMs);
+   const minutosAteEvento = Math.floor((distanciaAteEvento % horaEmMs) / minutosEmMS);
+   const segundosAteEvento = Math.floor((distanciaAteEvento % minutosEmMS) / 1000);
+
+   document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`;
+
+   if(distanciaAteEvento < 0){
+    clearInterval(contandoHoras);
+    document.getElementById('contador').innerHTML = `Evento EXPIRADO!`
+   }
+
+}, 1000);
